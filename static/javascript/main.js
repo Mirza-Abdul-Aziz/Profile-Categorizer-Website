5b04a5b133fdc41ce3e4c7de9e2bd06fcb9b3c62
Mirza-Abdul-Aziz <mirzaabdulaziz006@gmail.com>
2022-04-25 03:06:23 -0700

    Made a base template and inherited all other pages from it to reduce the code by copying it again and again in alll pages....

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,7 +1,9 @@
 var root = document.querySelector(':root')
+// var twitter_logo = document.getElementsByClassName('body')
 function light_theme() {
   root.style.setProperty("--body-color", "#d6d6d6");
   root.style.setProperty("--navbar-color", "lightgray");
+//   twitter_logo.style.setProperty("")
 }
 function dark_theme() {
   root.style.setProperty("--body-color", "black");
